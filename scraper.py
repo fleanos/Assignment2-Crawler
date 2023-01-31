e4265b376e33a9018cae582f3e52ee45cc00f102
Fernando Leanos <60533313+fleanos@users.noreply.github.com>
2023-01-31 04:05:12 +0000

    added a few extra url extensions to avoid

--- scraper.py
+++ scraper.py
@@ -148,7 +148,8 @@ def is_valid(url):
       r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names" +
       r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso" +
       r"|epub|dll|cnf|tgz|sha1" + r"|thmx|mso|arff|rtf|jar|csv" +
-      r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+      r"|rm|smil|wmv|swf|wma|zip|rar|gz|#respond|#comment" +
+      r"wp-content/uploads|?cal|?share=|?replytocom)$", parsed.path.lower())
 
   except TypeError:
     print("TypeError for ", parsed)
